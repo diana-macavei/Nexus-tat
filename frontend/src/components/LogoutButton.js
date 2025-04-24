38629a388d19e5a4d1d78519dc6b0066252b53ad
diana-macavei <macavei.diana@gmail.com>
2025-04-24 22:02:24 +0300

    added .gitignore and mb fixed_auth?

--- frontend/src/components/LogoutButton.js
+++ frontend/src/components/LogoutButton.js
@@ -1,4 +1,6 @@
 import { useNavigate } from "react-router-dom";
+import React from "react";
+
 
 const LogoutButton = () => {
   const navigate = useNavigate();
---
+++ frontend/src/pages/Unauthorized.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Unauthorized = () => {
+  return <h2 style={{ textAlign: "center", marginTop: "3rem" }}>🚫 You are not authorized to view this page.</h2>;
+};
+
+export default Unauthorized;
